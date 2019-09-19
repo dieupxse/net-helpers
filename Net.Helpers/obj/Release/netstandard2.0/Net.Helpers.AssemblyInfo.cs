6a0f796a3fb661f02d2a8a774d491f46d9afd3ea
Jacob Pham <dieupxse@gmail.com>
2019-09-19 13:25:14 +0700

    Update license file

--- Net.Helpers/obj/Release/netstandard2.0/Net.Helpers.AssemblyInfo.cs
+++ Net.Helpers/obj/Release/netstandard2.0/Net.Helpers.AssemblyInfo.cs
@@ -9,15 +9,8 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jacob Pham")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute(@"
-      This Lib contain huge of Helpers Functions:
-      SecurityHelper, StringHelper, EmailHelper, ImageHelper, LogHelper, CacheHelper, ApiHelper
-      Extensions:
-      DateTimeExtensions,  RestClientExtentions (RestSharp Helper Extensions)
-      Checkout Project And Find Out What You Need https://github.com/dieupxse/net-helpers.git
-    ")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.2")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.3.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Net.Helpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Net.Helpers")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.2.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.3.0")]
