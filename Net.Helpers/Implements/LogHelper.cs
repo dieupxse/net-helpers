d73b8beee0918815de9f8f98ac761b0e5c7a61ac
Jacob Pham <dieupxse@gmail.com>
2019-09-22 01:04:49 +0700

    Update => Add Config log path

--- Net.Helpers/Implements/LogHelper.cs
+++ Net.Helpers/Implements/LogHelper.cs
@@ -2,11 +2,15 @@ using System;
 using System.IO;
 using Net.Helpers.Interfaces;
 
-namespace Net.Helpers.Implements {
-    public class LogHelper : ILogHelper {
+namespace Net.Helpers.Implements
+{
+    public class LogHelper : ILogHelper
+    {
         private readonly IStringHelper _stringHelper;
-        public LogHelper () { }
-        public LogHelper (IStringHelper stringHelper) {
+        private string _logPath;
+        public LogHelper() { }
+        public LogHelper(IStringHelper stringHelper)
+        {
             _stringHelper = stringHelper;
         }
         /// <summary>
@@ -16,45 +20,56 @@ namespace Net.Helpers.Implements {
         /// <param name="logMessage">Content</param>
         /// <param name="type">Can be any string Ex: error, info, success, warning ...</param>
         /// <param name="className">Filter by ClassName</param>
-        public void Log (string logMessage, string type = "info", string className = "") {
-            try {
+        public void Log(string logMessage, string type = "info", string className = "")
+        {
+            try
+            {
                 string strLogMessage = string.Empty;
-                String fName = DateTime.Now.ToString ("yyyy_MM_dd") + ".txt";
+                String fName = $"{(!string.IsNullOrEmpty(type) ? $"{type}." : "")}{(!string.IsNullOrEmpty(className) ? $"{className}." : "")}{DateTime.Now.ToString("yyyy_MM_dd") }.txt";
                 StreamWriter swLog;
 
-                strLogMessage = string.Format ("[{0}]: {1}", DateTime.Now, logMessage);
-                var logItem = (!string.IsNullOrEmpty (className) ? $"{className}/" : "");
-                var originalDirectory = new DirectoryInfo ($"wwwroot/{logItem}Logs/{type}/");
-                string pathString = Path.Combine (originalDirectory.ToString (), DateTime.Now.ToString ("yyyy-MM"));
+                strLogMessage = string.Format("[{0}]: {1}", DateTime.Now, logMessage);
+                var logItem = (!string.IsNullOrEmpty(className) ? $"{className}/" : "");
+                var originalDirectory = new DirectoryInfo(!string.IsNullOrEmpty(_logPath) ? $"{_logPath}" : $"wwwroot/Logs/");
+                string pathString = Path.Combine(originalDirectory.ToString());
                 //var fileName1 = Path.GetFileName(fName);
                 //create directory if does not exist
-                bool isExists = Directory.Exists (pathString);
-                if (!isExists) Directory.CreateDirectory (pathString);
+                bool isExists = Directory.Exists(pathString);
+                if (!isExists) Directory.CreateDirectory(pathString);
                 //generate local path to save file
                 var path = $"{pathString}/{fName}";
-                if (!File.Exists (path)) {
+                if (!File.Exists(path))
+                {
 
-                    FileStream fs = new FileStream (path, FileMode.CreateNew);
-                    StreamWriter swr = new StreamWriter (fs);
-                    try {
-                        swr.Close ();
-                        fs.Close ();
-                    } catch (Exception) {
-                        swr.Close ();
-                        fs.Close ();
+                    FileStream fs = new FileStream(path, FileMode.CreateNew);
+                    StreamWriter swr = new StreamWriter(fs);
+                    try
+                    {
+                        swr.Close();
+                        fs.Close();
+                    }
+                    catch (Exception)
+                    {
+                        swr.Close();
+                        fs.Close();
                     }
                 }
 
-                if (!File.Exists (path)) {
-                    swLog = new StreamWriter (path);
-                } else {
-                    swLog = File.AppendText (path);
+                if (!File.Exists(path))
+                {
+                    swLog = new StreamWriter(path);
+                }
+                else
+                {
+                    swLog = File.AppendText(path);
                 }
 
-                swLog.WriteLine (strLogMessage);
-                swLog.WriteLine ();
-                swLog.Close ();
-            } catch (Exception) {
+                swLog.WriteLine(strLogMessage);
+                swLog.WriteLine();
+                swLog.Close();
+            }
+            catch (Exception)
+            {
 
             }
         }
@@ -64,8 +79,14 @@ namespace Net.Helpers.Implements {
         /// <param name="logMessage">Object content</param>
         /// <param name="type">Can be any string Ex: error, info, success, warning ...</param>
         /// <param name="className">Filter by ClassName</param>
-        public void Log (object logMessage, string type = "info", string className = "") {
-            Log (_stringHelper.SerializeCamelCase (logMessage), type, className);
+        public void Log(object logMessage, string type = "info", string className = "")
+        {
+            Log(_stringHelper.SerializeCamelCase(logMessage), type, className);
+        }
+
+        public void Setup(string logPath)
+        {
+            _logPath = logPath;
         }
     }
 }
--- Net.Helpers/Interfaces/ILogHelper.cs
+++ Net.Helpers/Interfaces/ILogHelper.cs
@@ -4,6 +4,7 @@ namespace Net.Helpers.Interfaces
 {
     public interface ILogHelper
     {
+        void Setup(string logPath);
         void Log(string logMessage, string type = "info", string className = "");
         void Log(object logMessage, string type = "info", string className = "");
     }
